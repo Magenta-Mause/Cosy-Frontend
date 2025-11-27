2467cf545434801ffdc4e5f5f1c878f90b71d2bb
fietensen <fietensen@gmail.com>
2025-11-27 14:07:16 +0100

    add automatic newline config for vscode

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,11 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router"
-import React from "react"
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import React from "react";
 
 const RootLayout = () => (
   <React.Fragment>
     {/* Configure application shell here  */}
     <Outlet />
   </React.Fragment>
-)
+);
 
-export const Route = createRootRoute({ component: RootLayout })
+export const Route = createRootRoute({ component: RootLayout });
