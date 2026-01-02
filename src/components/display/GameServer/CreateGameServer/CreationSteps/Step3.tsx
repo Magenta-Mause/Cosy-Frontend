4668535c1d92e6f18e530132b1d85ddd2f08946b
Eco_Fresh_Kaese <alexander.jablo@gmail.com>
2026-01-02 12:55:39 +0100

    chore: fix lint

--- src/components/display/GameServer/CreateGameServer/CreationSteps/Step3.tsx
+++ src/components/display/GameServer/CreateGameServer/CreationSteps/Step3.tsx
@@ -1,4 +1,4 @@
-import KeyValueInput, {} from "@components/display/GameServer/CreateGameServer/KeyValueInput.tsx";
+import KeyValueInput from "@components/display/GameServer/CreateGameServer/KeyValueInput.tsx";
 import { DialogDescription } from "@components/ui/dialog.tsx";
 import * as z from "zod";
 import useTranslationPrefix from "@/hooks/useTranslationPrefix/useTranslationPrefix.tsx";
