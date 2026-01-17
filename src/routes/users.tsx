64b30df1bc5ac2cdf28414e2bc1f3aae53d17f7a
MCLemuri <elefine@mail.de>
2026-01-17 22:41:16 +0100

    add basic userCard

--- src/routes/users.tsx
+++ src/routes/users.tsx
@@ -1,7 +1,11 @@
+import { UserList } from "@components/display/UserManagement/UserModal/UserList";
+import { Badge } from "@components/ui/badge";
 import { Button } from "@components/ui/button";
+import { Card, CardContent } from "@components/ui/card";
 import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { ArrowLeft } from "lucide-react";
 import { useTranslation } from "react-i18next";
+import { useTypedSelector } from "@/stores/rootReducer";
 
 export const Route = createFileRoute("/users")({
   component: UserDetailPage,
@@ -10,24 +14,28 @@ export const Route = createFileRoute("/users")({
 function UserDetailPage() {
   const { t } = useTranslation();
   const router = useRouter();
-
-
+  const users = useTypedSelector((state) => state.userSliceReducer.data);
 
   return (
     <div className="p-2">
       <Button
         onClick={() => {
           router.navigate({
-            to: '/',
+            to: "/",
           });
-        }}>
+        }}
+      >
         <ArrowLeft className="size-5" />
         Back
       </Button>
       <div className="container mx-auto py-20 flex flex-col gap-4">
         test yey
-
-
+        <Card>
+          <CardContent className="flex gap-2 items-center m-2">
+            User 1
+            <Badge>Admin</Badge>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
