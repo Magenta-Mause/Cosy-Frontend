ab39daa4884bb98551e5e74e9438d68cc48648c7
Janne Keipert <jabbekeipert@gmail.com>
2026-01-17 02:22:33 +0100

    refactor: update GameServer header border color to use `border-button-primary-default` for consistency

--- src/components/display/GameServer/GameServerDetailPageLayout/GameServerDetailPageHeader/GameServerDetailPageHeader.tsx
+++ src/components/display/GameServer/GameServerDetailPageLayout/GameServerDetailPageHeader/GameServerDetailPageHeader.tsx
@@ -4,7 +4,7 @@ import type { GameServerDto } from "@/api/generated/model";
 
 const GameServerDetailPageHeader = (props: { gameServer: GameServerDto }) => {
   return (
-    <div className="flex align-bottom items-end gap-12 border-b-4 border-gray-700 py-4 justify-between">
+    <div className="flex align-bottom items-end gap-12 border-b-4 border-button-primary-default py-4 justify-between">
       <div className={"text-5xl truncate text-ellipsis max-w-[45vw]"}>
         {props.gameServer.server_name}
       </div>
