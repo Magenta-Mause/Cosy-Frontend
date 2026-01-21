64fd942a9503d7b5eaf9e24dd4f9698d1b89e278
hyunjoonchoi <hyunjoonchoi04@gmail.com>
2026-01-21 23:21:32 +0100

    chore: added metrics to main detail page

--- src/routes/server/$serverId.index.tsx
+++ src/routes/server/$serverId.index.tsx
@@ -1,8 +1,11 @@
 import LogDisplay from "@components/display/LogDisplay/LogDisplay.tsx";
+import MetricDisplay from "@components/display/MetricDisplay/MetricDisplay";
+import MetricGraph from "@components/display/MetricDisplay/MetricGraph";
 import { createFileRoute } from "@tanstack/react-router";
 import { useTranslation } from "react-i18next";
 import useGameServer from "@/hooks/useGameServer/useGameServer.tsx";
 import useGameServerLogs from "@/hooks/useGameServerLogs/useGameServerLogs.tsx";
+import useGameServerMetrics from "@/hooks/useGameServerMetrics/useGameServerMetrics";
 
 export const Route = createFileRoute("/server/$serverId/")({
   component: GameServerDetailPageDashboardPage,
@@ -13,6 +16,7 @@ function GameServerDetailPageDashboardPage() {
   const { serverId } = Route.useParams();
   const gameServer = useGameServer(serverId ?? "");
   const { logs } = useGameServerLogs(serverId ?? "");
+  const { metrics } = useGameServerMetrics(serverId ?? "");
 
   if (!serverId || !gameServer) {
     return <div>{t("serverPage.notFound")}</div>;
@@ -21,7 +25,7 @@ function GameServerDetailPageDashboardPage() {
   return (
     <div className="container mx-auto flex flex-col gap-4 h-[30vh]">
       <div className="flex flex-row gap-2 grow">
-        <div className="grow bg-gray-500 h-full"></div>
+        <MetricGraph className="w-[50%]" unit={"hour"} type={"cpu_percent"} metrics={metrics} />
         <LogDisplay logMessages={logs} className="grow" />
       </div>
     </div>
