92cbae4eee0e993044d8884edae074b377ffe516
MCLemuri <elefine@mail.de>
2026-01-23 14:27:34 +0100

    improve invite Modal

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -32,7 +32,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(function Input({ cl
           {...props}
         />
         {endDecorator && (
-          <div className="pointer-events-none absolute inset-y-0 right-3 flex items-center text-muted-foreground">
+          <div className="pointer-events-none text-base absolute inset-y-0 right-3 flex items-center text-muted-foreground">
             {endDecorator}
           </div>
         )}
