7cab65242730affcb9645209cf80e2a792bd73f9
MCLemuri <elefine@mail.de>
2026-01-27 10:23:33 +0100

    add const USER_COLORS

--- src/components/display/UserManagement/UserDetailPage/UserRow.tsx
+++ src/components/display/UserManagement/UserDetailPage/UserRow.tsx
@@ -4,12 +4,18 @@ import { Card, CardContent } from "@components/ui/card";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@components/ui/tooltip";
 import { Ellipsis } from "lucide-react";
 import { useTranslation } from "react-i18next";
-import type { UserEntityDto, UserEntityDtoRole } from "@/api/generated/model";
+import { type UserEntityDto, UserEntityDtoRole } from "@/api/generated/model";
 import { cn } from "@/lib/utils";
 import ResourceUsageBadge from "./ResourceUsageBadge";
 
 const UserRow = (props: { user: UserEntityDto; userName: string; userRole: UserEntityDtoRole }) => {
   const { t } = useTranslation();
+
+  const USER_COLORS: Record<UserEntityDtoRole, string> = {
+    [UserEntityDtoRole.OWNER]: "bg-[#0eaf9b]",
+    [UserEntityDtoRole.ADMIN]: "bg-[#8ff8e2]",
+    [UserEntityDtoRole.QUOTA_USER]: "bg-white",
+  };
 
   return (
     <Card>
@@ -19,9 +25,7 @@ const UserRow = (props: { user: UserEntityDto; userName: string; userRole: UserE
           <Badge
             className={cn(
               "rounded-xl text-sm px-3 uppercase",
-              props.user.role === "OWNER" && "bg-[#0eaf9b]",
-              props.user.role === "ADMIN" && "bg-[#8ff8e2]",
-              props.user.role === "QUOTA_USER" && "bg-white",
+              USER_COLORS[props.user.role ?? "QUOTA_USER"],
             )}
           >
             {t(`components.userManagement.userRow.roles.${props.userRole.toLowerCase()}`)}
@@ -31,12 +35,20 @@ const UserRow = (props: { user: UserEntityDto; userName: string; userRole: UserE
           <div className="flex gap-3 flex-1 justify-end">
             <ResourceUsageBadge
               currentValue="2"
-              limit={props.user.max_cpu != null ? props.user.max_cpu / 1000 : t("components.userManagement.userRow.resources.unlimited")}
+              limit={
+                props.user.max_cpu != null
+                  ? props.user.max_cpu / 1000
+                  : t("components.userManagement.userRow.resources.unlimited")
+              }
               resourceType={t("components.userManagement.userRow.resources.cpus")}
             />
             <ResourceUsageBadge
               currentValue="4,3"
-              limit={props.user.max_memory != null ? props.user.max_memory : t("components.userManagement.userRow.resources.unlimited")}
+              limit={
+                props.user.max_memory != null
+                  ? props.user.max_memory
+                  : t("components.userManagement.userRow.resources.unlimited")
+              }
               unit={props.user.max_memory != null ? "MB" : undefined}
               resourceType={t("components.userManagement.userRow.resources.memory")}
             />
