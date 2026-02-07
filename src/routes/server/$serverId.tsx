fdfae68ad9b9e9c5a849402632f040e12d79dd30
Fiete Minge <fietensen@gmail.com>
2026-02-07 20:30:56 +0100

    refactor: outsource server 404 to own component

--- src/routes/server/$serverId.tsx
+++ src/routes/server/$serverId.tsx
@@ -1,8 +1,7 @@
 import GameServerDetailPageLayout from "@components/display/GameServer/GameServerDetailPageLayout/GameServerDetailPageLayout.tsx";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import useGameServer from "@/hooks/useGameServer/useGameServer.tsx";
-import useTranslationPrefix from "@/hooks/useTranslationPrefix/useTranslationPrefix";
-import { Button } from "@components/ui/button";
+import { GameServerNotFoundPage } from "@components/display/GameServer/GameServerNotFoundPage/GameServerNotFoundPage";
 
 export const Route = createFileRoute("/server/$serverId")({
   component: GameServerDetailPage,
@@ -11,24 +10,9 @@ export const Route = createFileRoute("/server/$serverId")({
 function GameServerDetailPage() {
   const { serverId } = Route.useParams();
   const { gameServer, initialized } = useGameServer(serverId ?? "");
-  const navigate = Route.useNavigate();
-  const { t } = useTranslationPrefix("serverPage");
 
   if (initialized && !gameServer) {
-    return (
-      <div className="flex flex-col items-center justify-center h-screen">
-        <div className="flex flex-col items-center gap-4">
-          <span className="text-xl">{t("notFound")}</span>
-          <Button
-            onClick={() => {
-              navigate({ to: "/" });
-            }}
-          >
-            {t("notFoundGoBack")}
-          </Button>
-        </div>
-      </div>
-    );
+    return <GameServerNotFoundPage />;
   }
 
   if (!gameServer) {
