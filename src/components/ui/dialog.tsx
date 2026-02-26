7a2aff7ff2eed3670f3c5b9eb2a6c65c9eeddb04
simon dietrich <github.difficult926@passmail.net>
2026-02-26 12:27:48 +0100

    chore: replace CrossIcon img with Icon component in Dialog and adjust header styling

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,8 +1,9 @@
 import * as React from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
-import CrossIcon from "@/assets/icons/CrossIcon.svg";
+import crossIcon from "@/assets/icons/CrossIcon.svg?raw";
 
 import { cn } from "@/lib/utils"
+import Icon from "@/components/ui/Icon"
 
 function Dialog({
   ...props
@@ -72,7 +73,7 @@ function DialogContent({
           data-slot="dialog-close"
           className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-7"
         >
-          <img src={CrossIcon} alt="close" className="size-6" />
+          <Icon src={crossIcon} viewBox="0 0 35 35" className="size-6" />
           <span className="sr-only">Close</span>
         </DialogPrimitive.Close>
       )}
@@ -91,7 +92,7 @@ function DialogHeader({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="dialog-header"
-      className={cn("flex flex-col gap-2 text-center sm:text-left", className)}
+      className={cn("flex flex-col gap-2 text-center sm:text-left pr-8", className)}
       {...props}
     />
   )
